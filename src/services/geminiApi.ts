919d8623fe9107f557702fe9fdf2f84407398f72
Ricardo Machado <ricardo.machado.nwi@gmail.com>
2025-03-17 09:07:12 -0300

    feat: update to gemini-flash 2

--- src/services/geminiApi.ts
+++ src/services/geminiApi.ts
@@ -1,4 +1,4 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY)
-export const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+export const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' })
